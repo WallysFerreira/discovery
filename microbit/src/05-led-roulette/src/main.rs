4e19cf1aa7d45e616b49fc44917fc67d5da501cd
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-07 14:53:01 -0300

    solve led roulette challenge

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -5,25 +5,61 @@
 use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
-use microbit::board::Board;
-use microbit::hal::timer::Timer;
-use microbit::hal::prelude::*;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{Timer, prelude::*}
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let mut board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let delay = 30;
 
-    board.display_pins.col1.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    let mut what_to_show = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
 
     loop {
-        row1.set_low().unwrap();
-        rprintln!("Apagado!");
-        timer.delay_ms(1_000_u16);
-        row1.set_high().unwrap();
-        rprintln!("Aceso!");
-        timer.delay_ms(1_000_u16);
+        for i in 0..9 {
+            if i == 0 {
+                for j in 0..5 {
+                    clearArray(&mut what_to_show);
+                    what_to_show[i][j] = 1;
+                    display.show(&mut timer, what_to_show, delay);
+                }
+            } else if i == 4 {
+                for j in (0..5).rev() {
+                    clearArray(&mut what_to_show);
+                    what_to_show[i][j] = 1;
+                    display.show(&mut timer, what_to_show, delay);
+                }
+            } else if i < 5 {
+                clearArray(&mut what_to_show);
+                what_to_show[i][4] = 1;
+                display.show(&mut timer, what_to_show, delay);
+            } else {
+                clearArray(&mut what_to_show);
+                what_to_show[4 - (i - 5)][0] = 1;
+                display.show(&mut timer, what_to_show, delay);
+            }
+        }
     }
+}
+
+fn clearArray(arr: &mut [[u8 ; 5] ; 5]) {
+    *arr = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
 }
