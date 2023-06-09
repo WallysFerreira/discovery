13b7ad9ad768e4ba7269069a0114c66bdfce5052
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-09 18:19:23 -0300

    read user command from serial

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -1,49 +1,56 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
+use core::fmt::Write;
+use core::str::from_utf8;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
+use heapless::Vec;
 
-use microbit::hal::timer::Timer;
-use microbit::hal::prelude::*;
-
-#[cfg(feature = "v2")]
 use microbit::{
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
     hal::twim,
     pac::twim0::frequency::FREQUENCY_A,
-    hal::uarte,
-    hal::uarte::{Baudrate, Parity}
 };
 
-mod serial_setup;
 use lsm303agr::{AccelOutputDataRate, Lsm303agr};
+
+mod serial_setup;
 use serial_setup::UartePort;
-
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
-
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
 
-    #[cfg(feature = "v2")]
-    let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
 
-    let mut sensor = Lsm303agr::new_with_i2c(i2c);
-    sensor.init().unwrap();
-    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    let mut buffer: Vec<u8, 32> = Vec::new();
     loop {
-        if sensor.accel_status().unwrap().xyz_new_data {
-            let data = sensor.accel_data().unwrap();
-            rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
-            timer.delay_ms(1000u16);
+        buffer.clear();
+        
+        loop {
+            let byte = nb::block!(serial.read()).unwrap();
+
+            if buffer.push(byte).is_err() {
+                write!(serial, "Erro lendo comando\r\n").unwrap();
+            }
+
+            if byte == 13 {
+                break;
+            }
         }
+
     }
 }
