e6129843041b5163c16aa05659c6fd04652c1cbb
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-09 19:05:16 -0300

    return accelerometer data when user types accelerometer

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -3,7 +3,6 @@
 
 use cortex_m_rt::entry;
 use core::fmt::Write;
-use core::fmt::format;
 use core::str::from_utf8;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
@@ -17,7 +16,7 @@ use microbit::{
     pac::twim0::frequency::FREQUENCY_A,
 };
 
-use lsm303agr::{AccelOutputDataRate, Lsm303agr};
+use lsm303agr::{AccelOutputDataRate, Lsm303agr, Measurement};
 
 mod serial_setup;
 use serial_setup::UartePort;
@@ -37,6 +36,11 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+
     let mut buffer: Vec<u8, 32> = Vec::new();
     loop {
         buffer.clear();
@@ -52,7 +56,8 @@ fn main() -> ! {
             if byte == 13 {
                 if from_utf8(&buffer).unwrap() == "accelerometer\r" {
                     write!(serial, "You typed accelerometer\r\n").unwrap();
-                    break;
+                    let data = sensor.accel_data().unwrap();
+                    write!(serial, "x {} y {} z {}\r\n", data.x, data.y, data.z).unwrap();
                 }
                 if from_utf8(&buffer).unwrap() == "magnetometer\r" {
                     write!(serial, "You typed magnetometer\r\n").unwrap();
@@ -63,16 +68,3 @@ fn main() -> ! {
     }
 }
 
-fn data_from_accel(board: microbit::Board) -> str {
-    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_interal.into(), FREQUENCY_A::k100) };
-    let mut sensor = Lsm303agr::new_with_i2c(i2c);
-    sensor.init().unwrap();
-    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
-
-    loop {
-        if sensor.accel_status().unwrap().xyz_new_data {
-            let data = sensor.accel_data().unwrap();
-            let string = format!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
-        }
-    }
-}
