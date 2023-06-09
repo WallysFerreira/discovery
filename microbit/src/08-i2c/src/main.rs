5e145dc251bd66c451e4ac5d992fa9708da21e03
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-09 17:42:38 -0300

    start configuring uarte port

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -6,21 +6,20 @@ use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
 
+use microbit::hal::timer::Timer;
 use microbit::hal::prelude::*;
-
-#[cfg(feature = "v1")]
-use microbit::{
-    hal::twi,
-    pac::twi0::frequency::FREQUENCY_A,
-};
 
 #[cfg(feature = "v2")]
 use microbit::{
     hal::twim,
     pac::twim0::frequency::FREQUENCY_A,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity}
 };
 
+mod serial_setup;
 use lsm303agr::{AccelOutputDataRate, Lsm303agr};
+use serial_setup::UartePort;
 
 const ACCELEROMETER_ADDR: u8 = 0b0011001;
 const MAGNETOMETER_ADDR: u8 = 0b0011110;
@@ -32,10 +31,7 @@ const MAGNETOMETER_ID_REG: u8 = 0x4f;
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
-
-
-    #[cfg(feature = "v1")]
-    let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+    let mut timer = Timer::new(board.TIMER0);
 
     #[cfg(feature = "v2")]
     let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
@@ -47,6 +43,7 @@ fn main() -> ! {
         if sensor.accel_status().unwrap().xyz_new_data {
             let data = sensor.accel_data().unwrap();
             rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+            timer.delay_ms(1000u16);
         }
     }
 }
